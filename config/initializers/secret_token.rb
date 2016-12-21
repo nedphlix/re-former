06e28375c123b0172afd3830c9aa855740dcf6ee
Sebastien Phlix <sebastien@typeform.com>
2016-12-21 14:27:32 +0100

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+ReFormer::Application.config.secret_key_base = 'a1094436e5c386e29e546b5407de5f52ee8ec2b8cb241a70f5947a3528256699312b43a7dbc2ed7514230d3556095af7aa258bee9d63c48e6d1be5c300b87d6c'
